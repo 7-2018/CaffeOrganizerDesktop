29232fcdb297c4a13bc1d512eff72987bb1bfd20
DESKTOP-E66HHQA\Marta <marta.m2000@yahoo.com>
2020-12-19 21:42:40 +0100

    Created 3 layers for Workers Table

---
+++ CaffeOrganizerDesktop/CaffeOrganizer/Form1.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaffeOrganizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WorkerBusiness wokrerBusiness = new WorkerBusiness();
+            List<CaffeWorker> caffeWorkers = wokrerBusiness.GetCaffeWorkers();
+            foreach(CaffeWorker caffeWorker in caffeWorkers)
+            {
+                listBox1.Items.Add(caffeWorker.ToString());
+            }
+        }
+    }
+}
