dc9a8478eb74559696aefdb8ecc003ebf0c429ce
DESKTOP-E66HHQA\Marta <marta.m2000@yahoo.com>
2020-12-21 20:56:50 +0100

    Commit 3 Table repository in data layer created

---
+++ CaffeOrganizerDesktop/BusinessLayer/TableBusiness.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class TableBusiness
+    {
+        private TableRepository tableRepository;
+        public TableBusiness()
+        {
+            this.tableRepository = new TableRepository();
+        }
+        public List<CaffeTable> getCaffeWorkers()
+        {
+            return this.tableRepository.GetCaffeTables();
+        }
+    }
+}
---
+++ CaffeOrganizerDesktop/DataLayer/Models/CaffeBill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class CaffeBill
+    {
+        private int bill_ID;
+        private int table_ID;
+        private int total_Price;
+        private DateTime date_And_Time;
+    }
+}
