98f0dd29627fdabc93d92f68a457ff376eb36579
DESKTOP-E66HHQA\Marta <marta.m2000@yahoo.com>
2020-12-21 22:39:12 +0100

    Commit 5 ArticleRepository created

---
+++ CaffeOrganizerDesktop/DataLayer/ArticleRepository.cs
@@ -0,0 +1,74 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class ArticleRepository
+    {
+        public string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CaffeOrganizerDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public List<CaffeArticle>GetCaffeArticles()
+        {
+            List<CaffeArticle> caffeArticles = new List<CaffeArticle>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = connection;
+                sqlCommand.CommandText = "Select * From Articles";
+                SqlDataReader dataReader = sqlCommand.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    caffeArticles.Add(new CaffeArticle(dataReader.GetInt32(0), dataReader.GetString(1), dataReader.GetInt32(2), dataReader.GetString(3)));
+                }
+                connection.Close();
+            }
+            return caffeArticles;
+        }
+        public int InsertCaffeArticle(CaffeArticle caffeArticle)
+        {
+            int result;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.CommandText = $"Insert into Articles(Article_ID, Name, Price, Packaging) values({caffeArticle.Article_ID},{caffeArticle.Name},{caffeArticle.Price},{caffeArticle.Packaging})";
+                result = sqlCommand.ExecuteNonQuery();
+            }
+            return result;
+        }
+        public int DeleteArticle(int articleid)
+        {
+            int result;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand(connectionString);
+                sqlCommand.CommandText = "Delete from Workers where Article_ID = @articleID";
+                sqlCommand.Parameters.AddWithValue("@articleID", articleid);
+                result = sqlCommand.ExecuteNonQuery();
+            }
+            return result;
+        }
+        public int UpdateArticle(CaffeArticle caffeArticle)
+        {
+            int result;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand(connectionString);
+                sqlCommand.CommandText = "Update Articles SET Name =@name, Price= @price, Packaging=@package where Article_ID = @articleID";
+                sqlCommand.Parameters.AddWithValue("@name", caffeArticle.Name);
+                sqlCommand.Parameters.AddWithValue("@price", caffeArticle.Price);
+                sqlCommand.Parameters.AddWithValue("@package", caffeArticle.Packaging);
+                sqlCommand.Parameters.AddWithValue("@articleID", caffeArticle.Article_ID);
+                result = sqlCommand.ExecuteNonQuery();
+            }
+            return result;
+        }
+    }
+}
