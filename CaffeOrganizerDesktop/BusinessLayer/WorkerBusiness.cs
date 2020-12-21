dd411f0d03e2563e86aefc7142f02387151871a4
marijakulic <99marijakulic@gmail.com>
2020-12-21 22:04:25 +0100

    Commit 1 - BusinessLayer (TableBusiness and WorkerBusiness edited)

--- CaffeOrganizerDesktop/BusinessLayer/WorkerBusiness.cs
+++ CaffeOrganizerDesktop/BusinessLayer/WorkerBusiness.cs
@@ -18,5 +18,29 @@ namespace BusinessLayer
             {
             return this.workerRepository.GetCaffeWorkers();
             }
+        public bool InsertCaffeWorker(CaffeWorker caffeWorker)
+        {
+            int result = this.workerRepository.InsertCaffeWorker(caffeWorker);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+        public bool DeleteWorker(int workerId)
+        {
+            int result = this.workerRepository.DeleteWorker(workerId);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+        public bool UpdateWorker(CaffeWorker caffeWorker)
+        {
+            int result = this.workerRepository.UpdateWorker(caffeWorker);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
