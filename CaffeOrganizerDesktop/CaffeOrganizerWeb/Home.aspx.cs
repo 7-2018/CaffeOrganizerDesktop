ab9a1d5a3e96be33f1b19cf69ab28ca4181f0818
Darko <redda999@gmail.com>
2020-12-26 21:17:26 +0100

    Commit 1 Login created in Web Application and homepage design created

---
+++ CaffeOrganizerDesktop/CaffeOrganizerWeb/Home.aspx.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace CaffeOrganizerWeb
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            TableBusiness tb = new TableBusiness();
+            foreach(CaffeTable c in tb.getCaffeTables())
+            {
+                HtmlGenericControl column = new HtmlGenericControl("div");
+                column.Attributes.Add("class", "col-3 text-center");
+              
+                if (c.Taken)
+                    column.InnerHtml += $"<p style='background:red'>{c.Table_ID} <br> {c.Number_Of_Seats}<p>";
+                else
+                    column.InnerHtml += $"<p style='background:green'>{c.Table_ID} <br> {c.Number_Of_Seats}<p>";
+                maindiv.Controls.Add(column);
+            }
+        }
+    }
+}
---
+++ CaffeOrganizerDesktop/CaffeOrganizerWeb/Login.aspx.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CaffeOrganizerWeb
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void login_ServerClick(object sender, EventArgs e)
+        {
+            WorkerRepository wr = new WorkerRepository();
+            foreach(CaffeWorker caffeWorker in wr.GetCaffeWorkers())
+            { 
+            if (txtuser.Value.Equals(caffeWorker.User_Name) && txtpass.Value.Equals(caffeWorker.Password))
+                Response.Redirect("default.aspx");
+            else
+                Label1.Visible = true;
+            }
+        }
+    }
+}
