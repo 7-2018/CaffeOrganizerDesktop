fec0d2348f21d3248646206439fc780c7fd1c754
marijakulic <99marijakulic@gmail.com>
2020-12-26 18:53:37 +0100

    Commit 3 - ArticleBusiness created and finished

---
+++ CaffeOrganizerDesktop/BusinessLayer/ArticleBusiness.cs
@@ -0,0 +1,47 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class ArticleBusiness
+    {
+        private ArticleRepository articleRepository;
+        public ArticleBusiness()
+        {
+            this.articleRepository = new ArticleRepository();
+        }
+        public List<CaffeArticle> GetCaffeArticles()
+        {
+            return this.articleRepository.GetCaffeArticles();
+        }
+        public bool InsertCaffeArticle(CaffeArticle caffeArticle)
+        {
+            int result = this.articleRepository.InsertCaffeArticle(caffeArticle);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+        public bool DeleteArticle(int articleId)
+        {
+            int result = this.articleRepository.DeleteArticle(articleId);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+        public bool UpdateArticle(CaffeArticle caffeArticle)
+        {
+            int result = this.articleRepository.UpdateArticle(caffeArticle);
+            if (result != 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
